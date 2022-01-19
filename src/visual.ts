d175667c007ed11c8b41cbd26fa54d2febf2a483
HM <hieumdd@gmail.com>
2022-01-19 13:45:24 +0700

    fix: Fixed null for value in tooltip

--- src/visual.ts
+++ src/visual.ts
@@ -107,24 +107,24 @@ export class Visual implements IVisual {
             return objectEnumeration;
         }
 
-        switch (objectName) {
-            case 'arc':
-                objectEnumeration.push({
-                    objectName: objectName,
-                    properties: {
-                        arcColor: this.settings.arc.arcColor,
-                    },
-                    propertyInstanceKind: {
-                        arcColor: VisualEnumerationInstanceKinds.ConstantOrRule,
-                    },
-                    altConstantValueSelector: null,
-                    selector: dataViewWildcard.createDataViewWildcardSelector(
-                        dataViewWildcard.DataViewWildcardMatchingOption
-                            .InstancesAndTotals,
-                    ),
-                });
-                break;
-        }
+        // switch (objectName) {
+        //     case 'arc':
+        //         objectEnumeration.push({
+        //             objectName: objectName,
+        //             properties: {
+        //                 arcColor: this.settings.arc.arcColor,
+        //             },
+        //             propertyInstanceKind: {
+        //                 arcColor: VisualEnumerationInstanceKinds.ConstantOrRule,
+        //             },
+        //             altConstantValueSelector: null,
+        //             selector: dataViewWildcard.createDataViewWildcardSelector(
+        //                 dataViewWildcard.DataViewWildcardMatchingOption
+        //                     .InstancesAndTotals,
+        //             ),
+        //         });
+        //         break;
+        // }
         return objectEnumeration;
     }
 
@@ -183,7 +183,9 @@ export class Visual implements IVisual {
             .showLabels(false)
             .tooltipTitle(({ label }: Node) => label)
             .tooltipContent(({ value }: Node) =>
-                value !== undefined ? value.toString() : 'null',
+                value !== undefined && value !== null
+                    ? value.toString()
+                    : 'null',
             )
             .radiusScaleExponent(1)
             .labelOrientation('angular')(this.div);
